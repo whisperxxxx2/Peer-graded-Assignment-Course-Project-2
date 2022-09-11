ada26c07b88db3c9b5357e1f3a672e30cbe21e3f
whisperxxxx2 <110135477+whisperxxxx2@users.noreply.github.com>
2022-09-12 00:54:54 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,20 @@
 
+
+##load data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+##start plotting
+NEI <- NEI[NEI$fips == "24510",]
+pmTotals <- aggregate(Emissions ~ year,NEI, sum)
+png(filename = "plot3.png")
+library(ggplot2)
+png3 <- ggplot(data = NEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  guides(fill=FALSE)+
+  facet_grid(cols=vars(type),scales = "free",space="free") + 
+  labs(x="Year", y="Total PM2.5 Emission (Tons)") + 
+  labs(title="Total PM2.5 emissions in Baltimore City during 1999-2008 by Source Type")
+print(png3)
+dev.off()
+
